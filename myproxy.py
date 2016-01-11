822a39b6fceb7ba2d79264640701cbba37d97d78
Carla Wilby <wlbcar002@myuct.ac.za>
2016-01-11 10:43:20 +0200

    Structure of proxy. No errors but not working.

--- myproxy.py
+++ myproxy.py
@@ -1,17 +1,16 @@
 """my very basic proxy using twisted.web.proxy.Proxy(HTTPChannel)"""
 
 from twisted.web import http, proxy
-from twisted.web.proxy import ProxyRequest
 from twisted.internet import reactor
 from twisted.internet.protocol import Protocol
 from twisted.internet.protocol import Factory
+from twisted.web import server, resource
 
-"""What does this do?
-f = http.HTTPFactory()
-f.protocol = ProxyFactory"""
+# blacklist of disallowed domains (can change this to ips later)
 
+blacklist = ["facebook.com", "twitter.com", "zombo.com"]
 
-# Set up proxy
+# 1. Set up proxy
 
 
 class ProxyFactory(http.HTTPFactory):
@@ -19,41 +18,58 @@ class ProxyFactory(http.HTTPFactory):
         return proxy.Proxy()
 
 
-# My attempt to subclass proxyRequest
+class Proxy(proxy.Proxy):
+    requestFactory = proxy.ProxyRequest
 
 
-class CheckProxyRequest(ProxyRequest):
+class ProxyRequest(proxy.ProxyRequest):
+    protocols = dict(http=proxy.ProxyClientFactory)
+
+
+class ProxyClientFactory(proxy.ProxyClientFactory):
+    protocol = proxy.ProxyClient
+
+
+# 2. Check request
+
+class CheckProxyRequest(proxy.ProxyRequest):
     def process(self):
-        self.checkHeaders()
-        return ProxyRequest.process(self)
-    def checkHeaders(self):
-        # how to read header and path?
-        self.requestHeaders
+        if self.path in blacklist:
+            site
 
+
+# 3. Return page for blocked site (if applicable)
+
+class notAllowed(resource.Resource):
+    isLeaf = True
+
+    def render_GET(self, request):
+        return "<html>Sorry, you are not allowed access here.</html>"
+
+
+"""class URL(Resource):
+    isLeaf = True
+
+    def getChild(self, name, request):
+        if name == '':
+            return self
+        return Resource.getChild(self, name, request)
+
+    def render_GET(self, request):
+        if self.
+    return request.prepath"""
 
 
 """class ResourceFactory(Resource):
 site = ProxyRequest.path
 print site"""
 
-# Setting up server
-
-
-class QOTD(Protocol):
-    def connectionMade(self):
-        self.transport.write(ProxyRequest.path)
-        self.transport.loseConnection()
-
-
-class QOTDFactory(Factory):
-    def buildProtocol(self, addr):
-        return QOTD()
-
 # Connect
-
+site = server.Site(notAllowed())
 reactor.listenTCP(8080, ProxyFactory())
 reactor.run()
 
 # TODO include timeOut = no of seconds and then resetTimeout
 # TODO include blacklist of ips
 # TODO get the right port
+# TODO Make deferreds
