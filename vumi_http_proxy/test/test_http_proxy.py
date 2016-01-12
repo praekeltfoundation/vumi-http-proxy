de10d82564101fb9c9f8c866cd04d8475dc7d122
Carla Wilby <wlbcar002@myuct.ac.za>
2016-01-12 14:48:11 +0200

    Twisted proxy tests

--- vumi_http_proxy/test/test_http_proxy.py
+++ vumi_http_proxy/test/test_http_proxy.py
@@ -3,20 +3,22 @@ from twisted.trial import unittest
 from twisted.test import proto_helpers
 
 
-class ProxyFactoryTestCase(unittest.TestCase):
+class CheckProxyRequestTestCase(unittest.TestCase):
     def setUp(self):
         factory = ProxyFactory()
         self.proto = factory.buildProtocol(('0.0.0.0', 0))
         self.tr = proto_helpers.StringTransport()
         self.proto.makeConnection(self.tr)
 
+    def _test(self, operation a, b, expected):
+        self.proto.dataReceived('%s %d %d\r\n' % (operation))
+        self.assertEqual(int(self.tr.value()), expected)
+
     def test_buildProtocol(self, addr):
         cfactory = ProxyFactory()
         result = ProxyFactory.buildProtocol()
         self.assertEqual(result, "something")
 
-
-class CheckProxyRequestTestCase(unittest.TestCase):
     def test_process(self):
         checkRequest = CheckProxyRequest()
         result = CheckProxyRequest.process()
