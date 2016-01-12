6618fa18f06cb1ab46843ad8f0438c8ac87bd521
Carla Wilby <wlbcar002@myuct.ac.za>
2016-01-12 10:02:02 +0200

    Errors in setup.py addressed

--- setup.py
+++ setup.py
@@ -7,33 +7,25 @@ try:
 except ImportError:
     from distutils.core import setup
 
-
-with open('README.rst') as readme_file:
+with open('README.md') as readme_file:
     readme = readme_file.read()
-
-with open('requirements.txt') as req_file:
-    requirements = req_file.read().split('\n')
-
-with open('requirements-dev.txt') as req_file:
-    requirements_dev = req_file.read().split('\n')
-
-with open('VERSION') as fp:
-    version = fp.read().strip()
 
 setup(
     name="vumi-http-proxy",
-    version=version,
+    version="0.1",
     description="HTTP Proxy in Python Twisted to prevent unauthorized access to blacklisted ips",
     long_description=readme,
-    author="Carla Wilby",
-    author_email='thisiscarlawilby@gmail.co.za',
+    author="Praekelt Foundation",
+    author_email='dev@praekeltfoundation.org',
     url='https://github.com/praekelt/vumi-http-proxy',
     packages=[
         find_packages(),
     ],
-    package_dir={'': 'src'},
     include_package_data=True,
-    install_requires=requirements,
+    install_requires=[
+         'vumi',
+         'Twisted',
+    ],
     license="BSD",
     classifiers=[
         'Intended Audience :: Developers',
