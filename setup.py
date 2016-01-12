35a820b99f7117ecb737fb8416cdaccf130d4e9b
Carla Wilby <wlbcar002@myuct.ac.za>
2016-01-12 11:22:12 +0200

    More minor errors fixed

--- setup.py
+++ setup.py
@@ -1,29 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
 
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
+
 setup(
     name="vumi-http-proxy",
     version="0.1",
-    description="HTTP Proxy in Python Twisted to prevent unauthorized access to blacklisted ips",
+    description=(
+        "HTTP Proxy in Python Twisted to prevent unauthorized access to "
+        "blacklisted ips"),
     long_description=readme,
     author="Praekelt Foundation",
     author_email='dev@praekeltfoundation.org',
     url='https://github.com/praekelt/vumi-http-proxy',
-    packages=[
-        find_packages(),
-    ],
+    packages=find_packages(),
     include_package_data=True,
     install_requires=[
-         'vumi',
          'Twisted',
     ],
     license="BSD",
