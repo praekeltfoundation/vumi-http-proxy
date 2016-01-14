d08e97da944f3bccbde29d7c25ed15e2254009de
Carla Wilby <wlbcar002@myuct.ac.za>
2016-01-14 11:13:54 +0200

    deny and allow tests

--- vumi_http_proxy/test/test_http_proxy.py
+++ vumi_http_proxy/test/test_http_proxy.py
@@ -15,7 +15,17 @@ class CheckProxyRequestTestCase(unittest.TestCase):
         self.tr = proto_helpers.StringTransport()
         self.proto.makeConnection(self.tr)
 
-    def test_artisanal(self):
+    def test_artisanal_allow(self):
+        self.proto.dataReceived("\r\n".join([
+            "GET http://zombie.com/ HTTP/1.1",
+            "Host: zombie.com",
+            "",
+            "",
+        ]))
+        self.assertEqual(self.tr.value(), "")
+        # self.addCleanup(self.tr.loseConnection)
+
+    def test_artisanal_deny(self):
         self.proto.dataReceived("\r\n".join([
             "GET http://zombo.com/ HTTP/1.1",
             "Host: zombo.com",
@@ -34,9 +44,10 @@ class CheckProxyRequestTestCase(unittest.TestCase):
         ]))
 
     @inlineCallbacks
-    def test_with_client(self):
-        server_endpnt = serverFromString(reactor, "tcp:0:interface=127.0.0.1")
-        server = yield server_endpnt.listen(ProxyFactory())
+    def test_with_client_deny(self):
+        server_endpoint = serverFromString(
+            reactor, "tcp:0:interface=127.0.0.1")
+        server = yield server_endpoint.listen(ProxyFactory())
         self.addCleanup(server.stopListening)
         port = server.getHost().port
 
@@ -46,5 +57,26 @@ class CheckProxyRequestTestCase(unittest.TestCase):
         response = yield agent.request("GET", "http://zombo.com/")
         response_body = yield readBody(response)
 
-        self.assertEqual(response.code, 400)  # TODO: This should be 400.
+        self.assertEqual(response.code, 400)
         self.assertEqual(response_body, "<html>Denied</html>")
+
+    @inlineCallbacks
+    def test_with_client_allow(self):
+        server_endpoint = serverFromString(
+            reactor, "tcp:0:interface=127.0.0.1")
+        server = yield server_endpoint.listen(ProxyFactory())
+        self.addCleanup(server.stopListening)
+        port = server.getHost().port
+
+        client_endpoint = clientFromString(
+            reactor, "tcp:host=localhost:port=%s" % (port,))
+        agent = ProxyAgent(client_endpoint)
+        response = yield agent.request("GET", "http://zombie.com/")
+        response_body = yield readBody(response)
+
+        self.assertEqual(response.code, 200)
+        self.assertEqual(
+            response_body, "<html><FRAMESET border='0' "
+                           "ROWS='*,1'><FRAME SRC='http://dsnextgen.com/?"
+                           "a_id=&drid=&domainname=referer_detect'><FRAME "
+                           "SRC='blank.html'> </FRAMESET> </html>")
