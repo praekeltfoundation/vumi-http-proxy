63358423e02de51a4daa7b9415a0bb7c7e00be15
Carla Wilby <wlbcar002@myuct.ac.za>
2016-01-18 14:53:39 +0200

    Twistee plugin

---
+++ twisted/plugins/proxy_worker_starter.py
@@ -0,0 +1,3 @@
+from vumi_http_proxy.servicemaker import ProxyWorkerServiceMaker
+
+proxy_worker = ProxyWorkerServiceMaker()
---
+++ vumi_http_proxy/servicemaker.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from zope.interface import implements
+
+from twisted.python import usage
+from twisted.plugin import IPlugin
+from twisted.application.service import IServiceMaker
+from vumi_http_proxy.http_proxy import ProxyFactory
+from twisted.internet.endpoints import serverFromString
+from twisted.internet import reactor
+
+
+class Options(usage.Options):
+    optParameters = [["port", None, 8080,
+                     "The port number to start the proxy"],
+                     ["interface", None, "0.0.0.0", "IP to start proxy on"]]
+
+
+class ProxyWorkerServiceMaker(object):
+    implements(IServiceMaker, IPlugin)
+    tapname = "proxy_worker"
+    description = "Service maker to launch proxy from given port and ip addr"
+    options = Options
+
+    def makeService(self, options):
+        """ Call Initialize to start endpoint server """
+        factory = ProxyFactory(["asdf.com"])
+        endpoint = serverFromString(
+            reactor, "tcp:%d:interface=%s" % (options["port"],
+                                              str(options["interface"])))
+        endpoint.listen(factory)
+        return endpoint.listen(factory)
