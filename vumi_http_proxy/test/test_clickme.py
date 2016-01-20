060f2a7c955d70bfa9cbad83f0fc229699332cd6
Carla Wilby <wlbcar002@myuct.ac.za>
2016-01-20 12:09:14 +0200

    Clickme patch

--- vumi_http_proxy/test/test_clickme.py
+++ vumi_http_proxy/test/test_clickme.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
-# from click.testing import CliRunner
-from vumi_http_proxy import clickme
+from click.testing import CliRunner
+from vumi_http_proxy import clickme, http_proxy
 from twisted.trial import unittest
-from twisted.trial.unittest import SynchronousTestCase
 
 # Testing segment
 
 
 class TestClickMe(unittest.TestCase):
     def test_click(self):
-        # runner = CliRunner()
-        # import pdb; pdb.set_trace()
-        result = SynchronousTestCase.patch(
-                                           clickme.cli, clickme.cli.port, 8000)
+        runner = CliRunner()
+        self.patch(http_proxy.Initialize, 'main',
+                   lambda x: x)
+        result = runner.invoke(clickme.cli)
         self.assertEqual(result.exit_code, 0)
-        self.assertEqual(str(result.output).splitlines()[0], (
-                         'Starting connection to 0.0.0.0:8080'))
+        self.assertEqual(result.output.splitlines(), [
+           'Starting connection to 0.0.0.0:8080',
+        ])
