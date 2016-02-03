a5ca6e0a466cf3c4b77ce7b0bfc1cf0a9ecc533e
Carla Wilby <wlbcar002@myuct.ac.za>
2016-02-03 10:01:30 +0200

    Blacklist config file format

---
+++ vumi_http_proxy/proxy_blacklist.py
@@ -0,0 +1,13 @@
+"""Config file for vumi_http_proxy.
+    Add all ip addresses to blacklist here"""
+
+PROXY_BLACKLIST = [
+
+    # IPv4 ADDRESSES
+    '69.16.230.117',
+    '93.184.216.34',
+
+    # IPv6 ADDRESSES
+    '2606:2800:220:1:248:1893:25c8:1946'
+
+]
