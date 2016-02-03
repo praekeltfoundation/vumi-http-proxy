e35ff2f0e45289c40a57c9488156829c60f9d3a0
Carla Wilby <wlbcar002@myuct.ac.za>
2016-02-03 12:53:11 +0200

    Change unicode ip to string

--- vumi_http_proxy/clickme.py
+++ vumi_http_proxy/clickme.py
@@ -8,9 +8,8 @@ from vumi_http_proxy import http_proxy
 @click.option('--interface', default="0.0.0.0", help='eg 0.0.0.0')
 @click.option('--port', default=8080, help='eg 80')
 def cli(interface, port):
-    cli.interface = str(interface)
-    cli.port = port
     """This script runs vumi-http-proxy on <interface>:<port>"""
+    interface = str(interface)
     click.echo("Starting connection to %s:%d" % (interface, port))
     i = http_proxy.Initialize(["asdf.com"], interface, port)
     i.main()
